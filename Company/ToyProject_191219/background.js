492bb4933c7e4c877f23ab8163d49a3eca04ecdf
ssorrychoi <zzaesung17@gmail.com>
2019-12-20 10:24:27 +0900

    20191219

---
+++ Company/ToyProject_191219/background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "www.naver.com" }
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "developer.chrome.com" }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+});

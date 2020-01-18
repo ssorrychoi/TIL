e759aad3d57b522b8a9e06e3e3fb504073cacacb
ssorrychoi <zzaesung17@gmail.com>
2020-01-18 14:23:40 +0900

    Todo App

--- Company/2020/01/Vue/DAY4/practice/src/components/TodoFooter.vue
+++ Company/2020/01/Vue/DAY4/practice/src/components/TodoFooter.vue
@@ -1,22 +1,30 @@
 <template>
-  <div>
-    Footer
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo">ClearAll</span>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  methods: {
+    clearTodo() {
+      this.$emit("clearItem");
+    }
+  }
+};
+</script>
 
 <style scoped>
-  .clearAllContainer{
-    width: 8.5rem;
-    height: 50px;
-    line-height: 50px;
-    background-color: white;
-    border-radius: 5px;
-    margin: 0 auto;
-  }
-  .clearAllBtn{
-    color: #e20303;
-    display:block;
-  }
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
 </style>

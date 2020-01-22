e2b5f7f074ac201192367d6a679a4365f790602e
ssorrychoi <zzaesung17@gmail.com>
2020-01-22 20:02:51 +0900

    0122 commit

--- Company/2020/01/Test0121/test/src/components/HelloWorld.vue
+++ Company/2020/01/Test0121/test/src/components/HelloWorld.vue
@@ -1,38 +1,79 @@
 <template>
   <div>
     <b-container class="bv-example-row">
-      <div class="cmt-head">
-        코멘트 <small class="cmthead-small">총 1개의 코멘트가 있습니다.</small>
+      <div class="comment-head">
+        코멘트 <small class="comment-head-small">총 1개의 코멘트가 있습니다.</small>
       </div>
-      <div class="cmt-submit">
-        <textarea class="cmt-body" placeholder="※ 작품과 무관한 코멘트, 악플은 삭제 될 수 있습니다."></textarea>
-        <b-button class="cmtbody-btn" v-on:click="addComment" variant="outline-primary">등록하기</b-button>
+      <div class="comment-input">
+        <textarea class="comment-body" placeholder="※ 작품과 무관한 코멘트, 악플은 삭제 될 수 있습니다."></textarea>
+        <b-button class="comment-body-btn" v-on:click="addComment" variant="outline-primary">등록하기</b-button>
       </div>
       <ul class="comment-list">
-        <li class="commentlist-box" v-bind:key="comments.id" v-for="item in comments">
-
+        <li class="comment-list-box" v-bind:key="comments.id" v-for="item in comments">
           <div>
             <div class="flex-box">
-              <div class="list-avatar">
-                <img src="http://cf.joara.com/user_file/default_01.png">
+              <div class="user-avatar">
+                <img class="avatar-img"
+                     src="https://images.unsplash.com/photo-1579603808872-59daea6f889c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80">
               </div>
-
               <div class="user-info">
                 <p>
                   <span>{{item.name}}</span>
                   <span class="date">{{item.date}}</span>
+                  <span v-on:click="recomment"><a href="#">Reply</a></span>
                 </p>
                 <p class="comment">{{item.text}}</p>
               </div>
-              <div>
-                <span class="report-div">
-                  <a class="report" href="#">Report</a>
-                </span>
-              </div>
+              <span class="report-div">
+                <a class="report" href="#">Report</a>
+              </span>
             </div>
           </div>
+
+          <div class="recomment-box">
+            <div class="recomment-flex-box">
+              <div class="user-avatar">
+                <img class="avatar-img"
+                     src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg">
+              </div>
+              <div class="user-info">
+                <p>
+                  <span>{{item.name}}</span>
+                  <span class="date">{{item.date}}</span>
+                  <span><a href="#">Reply</a></span>
+                </p>
+                <p class="comment"><span class="reply-nickname">@{{item.name}}</span>{{item.text}}</p>
+              </div>
+              <span class="report-div">
+                <a class="report" href="#">Report</a>
+              </span>
+            </div>
+          </div>
+
+          <div class="recomment-box">
+            <div class="recomment-flex-box">
+              <div class="user-avatar">
+                <img class="avatar-img"
+                     src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg">
+              </div>
+              <div class="user-info">
+                <p>
+                  <span>{{item.name}}</span>
+                  <span class="date">{{item.date}}</span>
+                  <span><a href="#">Reply</a></span>
+                </p>
+                <p class="comment"><span class="reply-nickname">@{{item.name}}</span>{{item.text}}</p>
+              </div>
+              <span class="report-div">
+                <a class="report" href="#">Report</a>
+              </span>
+            </div>
+          </div>
+
         </li>
       </ul>
+
+
     </b-container>
   </div>
 
@@ -44,7 +85,7 @@
     data() {
       return {
         comments: [],
-        item: 1
+        item: 1,
       }
     },
     methods: {
@@ -55,71 +96,79 @@
           "date": "2020-01-01 21:00:00",
           "thumbnail": "https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg",
           "text": "HI :)",
+          "reply": {
+            "id": 2,
+            "name": "test3",
+            "date": "2020-01-01 21:00:00",
+            "thumbnail": "https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg",
+            "text": "HI2 :)"
+          },
         }
         this.comments.push(arr2);
-      }
+        console.log(this.comments);
+
+      },
+      recomment() {
+      },
     },
+
     created() {
       let arr = {
         "id": 0,
         "name": "test1",
         "date": "2020-01-01 21:00:00",
         "thumbnail": "https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg",
-        "text": "hello~",
+        "text": "hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~hello~",
+        "reply": [],
       };
       this.comments.push(arr);
-      console.log(this.comments);
     }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .cmt-head {
-    font-size: 2rem;
+  .comment-head {
+    font-size: 20px;
     padding-bottom: 5px;
   }
 
-  .cmthead-small {
+  .comment-head-small {
     color: #777
   }
 
-  .cmt-submit {
+  .comment-input {
     padding-bottom: 10px;
+    height: 50px;
   }
 
-  .cmt-body {
+  .comment-body {
     width: calc(100% - 100px);
-    height: 50px;
+    height: 100%;
     border: 1px solid #d0d0d0;
     color: #666;
-    font-family: Dotum, '돋움', Arial, sans-serif;
     padding: 5px;
   }
 
-  .cmtbody-btn {
+  .comment-body-btn {
     vertical-align: top;
-    width: 90px;
-    height: 50px;
+    width: 97px;
+    height: 100%;
+    padding: 0;
   }
 
   .comment-list {
-    display: block;
-    list-style: disc;
     border-top: 1px solid #d6d6d6;
     border-bottom: 1px solid #d6d6d6;
     padding: 0
   }
 
-  .commentlist-box {
+  .comment-list-box {
     padding: 12px 0px;
     border-bottom: 1px solid #e3e3e3;
     background-color: #f8f8f8;
     list-style: none;
-  }
 
-  .cmt-list {
-    padding-top: 3px;
   }
 
   .user-info {
@@ -128,6 +177,7 @@
     font-size: 12px;
     padding-left: 10px;
     width: 100%;
+
   }
 
   .date {
@@ -139,33 +189,45 @@
   .report {
     color: red;
     padding-right: 10px;
+    font-size: 10px;
+    font-weight: normal;
   }
 
   .comment {
     font-size: 12px;
     color: #747474;
     line-height: 18px;
-    overflow-x: hidden;
     word-break: break-all;
-
-  }
-
-  a:link {
-    text-decoration: none;
   }
 
   .flex-box {
     display: flex;
+
+  }
+  .recomment-flex-box{
+    display: flex;
   }
 
-  .list-avatar {
+  .user-avatar {
     padding-left: 10px;
+  }
 
+  .avatar-img {
+    width: 60px;
+    height: 60px;
   }
 
   .report-div {
     float: right;
     justify-content: flex-end;
   }
-
+  .reply-nickname{
+    font-weight: bold;
+    color: #1c7e9f;
+    font-size: 12px;
+  }
+  .recomment-box{
+    padding-left: 40px;
+    padding-top: 15px;
+  }
 </style>
